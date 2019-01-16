62c984466a8b8be8c143087a597c2f339dea04e4
virga42 <tcallaha@gmail.com>
2019-01-16 17:29:50 -0500

    diagonal difference

--- sample.py
+++ sample.py
@@ -6,3 +6,20 @@ def addition(x, y):
         return addition((x - 1), (y + 1))
 
 #print(addition(3, 5))
+
+arr = ( (11,  2,   4), 
+        ( 4,  5,   6),
+        (10,  8, -12) )
+
+def abs_diff(x, y):
+    return abs(x - y)
+
+def dd(arr):
+    matrix_size = len(arr)
+    total_diag_back = 0
+    total_diag_forward = 0
+    for i in range(0, matrix_size):
+        total_diag_back += arr[i][i]
+        total_diag_forward += arr[i][matrix_size-i-1]
+    return abs_diff(total_diag_back, total_diag_forward)
+
--- test_cases.py
+++ test_cases.py
@@ -1,9 +1,25 @@
 import unittest
-from sample import addition
+from sample import addition, dd
 
 class TestOperators(unittest.TestCase):
     def test_additon(self):
         self.assertEqual(addition(1, 2), 3)
+
+class TestDiagonalDifference(unittest.TestCase):
+    """ 
+    Given a square matrix, calculate the absolute difference between the sums of its diagonals.
+    https://www.hackerrank.com/challenges/diagonal-difference/problem
+    """
+    def setUp(self):
+        """ Setting up for the test """
+        self.arr = ( (11,  2,   4), 
+                ( 4,  5,   6),
+                (10,  8, -12) )
+    
+    def test_dd(self):
+        self.assertEqual(dd(self.arr), 15)
+        
+        
 
 if __name__ == '__main__':
     unittest.main()
